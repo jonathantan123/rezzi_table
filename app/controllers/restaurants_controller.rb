593144984c3b215bd04ab618918f89639febd328
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-10-24 19:31:29 -0400

    start restaurant styling

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,9 +8,11 @@ class RestaurantsController < ApplicationController
     def show
         @restaurant = Restaurant.find(params[:id])
         @reservation_times = @restaurant.reservations.map {|reservation| reservation.reservation_time}
-
         @times = ["12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm", "4:30pm", "5:00pm", "5:30pm", "6:00pm", "6:30pm", "7:00pm", "7:30pm", "8:00pm", "8:30pm", "9:00pm", "9:30pm", "10:00pm", "10:30pm", "11:00pm"]
-
+        # all restaurant reviews
+        @reviews = @restaurant.reviews
+        # average restaurant rating
+        @number_of_reviews = @reviews.length
     end
 
 
