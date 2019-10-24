346eaf109d8bda259193997627bf99a4039b340a
Jonathan Tan <you@example.com>
2019-10-24 17:27:45 -0400

    added search bar

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: "login"
   post '/login', to: 'sessions#create', as: "sessions"
   get 'signup', to: "users#new", as: "signup"
-  post "/logout", to: "sessions#destroy", as: "logout"
+  get "/logout", to: "sessions#destroy", as: "logout"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
